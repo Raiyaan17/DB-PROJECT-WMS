f132831c1bdc5b3cfc8ef03816aed6fe8f1006d9
Raiyaan Junaid Hamid <raiyaanjhamid@gmail.com>
2025-12-12 19:59:31 +0500

    more random commits

--- api/obj/Debug/net10.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net10.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cc1148e479c6e9255e38899048cf36858f441c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86575fc0369e8a98f7cc81603372a778376d32b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
